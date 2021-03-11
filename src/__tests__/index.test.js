95ad075a65ec36843448280f0e45f67c9d2afe91
Name <email@example.ru>
2021-03-11 21:29:20 +0300

    added a test with an incorrect value and a corner test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,12 +1,20 @@
 const countDown = require('../index.js');
 
-describe('count',()=>{
-    it('count down numbers', ()=>{
+describe('count down',()=>{
+    it('count down numbers 7', ()=>{
         const result = countDown(7)
         expect(result).toBe(7, 6, 5, 4 ,3, 2 ,1)
     })
-    it('count down numbers', ()=>{
+    it('count down numbers 2', ()=>{
         const result = countDown(2)
         expect(result).toBe(2 ,1)
+    })
+    it('string received', ()=>{
+        const result = countDown("fgh")
+        expect(result).toBe("Значение введено не верно! Введите число")
+    })
+    it('string received', ()=>{
+        const result = countDown(-5)
+        expect(result).toBe("Введите число больше 1")
     })
 })
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function countDown(n) {
+	if (typeof n != "number") return "Значение введено не верно! Введите число";
+	if (n < 1) return "Введите число больше 1"
 	for (let i = n; i >= 1; --i) {
   	return(i);
   } 
